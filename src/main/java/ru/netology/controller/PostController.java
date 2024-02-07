30b893c66a117d7f012392d4cb860ccc5efa31fe
Fyodor Parfyonov <parfyonov.fyodor@gmail.com>
2024-02-07 23:05:02 +0300

    Migrated to Spring MVC

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,44 +1,38 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Reader;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-  private static final String APPLICATION_JSON = "application/json";
   private final PostService service;
-  private static final Gson gson = new Gson();
 
   public PostController(PostService service) {
     this.service = service;
   }
 
-  public void all(HttpServletResponse response) throws IOException {
-    write(service.all(), response);
+  @GetMapping
+  public List<Post> all() {
+    return service.all();
   }
 
-  public void getById(long id, HttpServletResponse response) throws IOException {
-    //deserialize request & serialize response
-    write(service.getById(id), response);
+  @GetMapping("/{id}")
+  public Post getById(@PathVariable long id) {
+    return service.getById(id);
   }
 
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    final var post = gson.fromJson(body, Post.class);
-    write(service.save(post), response);
+  @PostMapping
+  public Post save(@RequestBody Post post) {
+    return service.save(post);
   }
 
-  public void removeById(long id, HttpServletResponse response) throws IOException {
-    //deserialize request & serialize response
+  @DeleteMapping("/{id}")
+  public void removeById(@PathVariable long id) {
     service.removeById(id);
-    write("Post " + id + " has been removed", response);
-  }
-
-  private void write (Object data, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    response.getWriter().print(gson.toJson(data));
   }
 }
